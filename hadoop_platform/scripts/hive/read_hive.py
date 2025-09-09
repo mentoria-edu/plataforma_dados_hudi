e7a0be044ca27616f7385e4747a8e9401d33efb2
Leo-Adelmo <leonardo-adelmo@hotmail.com>
2025-09-08 21:41:26 -0300

    Commit para backup. Esta branch será deletada

--- hadoop_platform/scripts/hive/read_hive.py
+++ hadoop_platform/scripts/hive/read_hive.py
@@ -11,12 +11,5 @@ spark.sql("SHOW DATABASES").show()
 spark.sql("SHOW TABLES").show()
 spark.table("gold.data_bureau").show()
 
-# (
-#     df.coalesce(1)  # força apenas 1 arquivo
-#       .write
-#       .option("header", True)        # inclui cabeçalho
-#       .option("encoding", "UTF-8")   # garante UTF-8
-#       .mode("overwrite")             # sobrescreve saída se já existir
-#       .csv("/opt/scripts/gold.csv")  # caminho local (vai criar diretório)
-# )
+
 
